d6b1ee9ce93a833ae229bda50324e83cffff2312
Kevin <kevin-lpz10@hotmail.com>
2022-08-12 08:45:35 -0500

    Examen 2

---
+++ src/code/examen2/maven-web-student-tracker2/src/main/java/com/luv2code/web/jdbc/StudentDbUtil.java
@@ -0,0 +1,230 @@
+package com.luv2code.web.jdbc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+public class StudentDbUtil {
+
+	private DataSource dataSource;
+
+	public StudentDbUtil(DataSource theDataSource) {
+		dataSource = theDataSource;
+	}
+	
+	public List<Student> getStudents() throws Exception {
+		
+		List<Student> students = new ArrayList<>();
+		
+		
+		try(Connection myConn=dataSource.getConnection();
+				Statement myStmt=myConn.createStatement();
+				ResultSet myRs= myStmt.executeQuery("select * from estudiante")) {
+			// process result set
+			while (myRs.next()) {
+				
+				// retrieve data from result set row
+				int id = myRs.getInt("id");
+				String nombre = myRs.getString("nombre");
+				String apellido = myRs.getString("apellido");
+				String email = myRs.getString("email");
+				String escuela = myRs.getString("escuela");
+				
+				// create new student object
+				Student tempStudent = new Student(id, nombre, apellido, email, escuela);
+				
+				// add it to the list of students
+				students.add(tempStudent);				
+			}
+			
+			return students;		
+		}
+		
+	}
+
+	private void close(Connection myConn, Statement myStmt, ResultSet myRs) {
+
+		try {
+			if (myRs != null) {
+				myRs.close();
+			}
+			
+			if (myStmt != null) {
+				myStmt.close();
+			}
+			
+			if (myConn != null) {
+				myConn.close();   // doesn't really close it ... just puts back in connection pool
+			}
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+	public void addStudent(Student theStudent) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+			
+			// create sql for insert
+			String sql = "insert into estudiante "
+					   + "(nombre, apellido, email, escuela) "
+					   + "values (?, ?, ?, ?)";
+			
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set the param values for the student
+			myStmt.setString(1, theStudent.getNombre());
+			myStmt.setString(2, theStudent.getApellido());
+			myStmt.setString(3, theStudent.getEmail());
+			myStmt.setString(4, theStudent.getEscuela());
+			
+			// execute sql insert
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}	
+	}
+
+	public Student getStudent(String theStudentId) throws Exception {
+
+		Student theStudent = null;
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		ResultSet myRs = null;
+		int studentId;
+		
+		try {
+			// convert student id to int
+			studentId = Integer.parseInt(theStudentId);
+			
+			// get connection to database
+			myConn = dataSource.getConnection();
+			
+			// create sql to get selected student
+			String sql = "select * from estudiante where id=?";
+			
+			// create prepared statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setInt(1, studentId);
+			
+			// execute statement
+			myRs = myStmt.executeQuery();
+			
+			// retrieve data from result set row
+			if (myRs.next()) {
+				String nombre = myRs.getString("nombre");
+				String apellido = myRs.getString("apellido");
+				String email = myRs.getString("email");
+				String escuela = myRs.getString("escuela");
+				
+				// use the studentId during construction
+				theStudent = new Student(studentId, nombre, apellido, email, escuela);
+			}
+			else {
+				throw new Exception("Could not find student id: " + studentId);
+			}				
+			
+			return theStudent;
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}	
+	}
+
+	public void updateStudent(Student theStudent) throws Exception {
+		
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+
+		try {
+			// get db connection
+			myConn = dataSource.getConnection();
+			
+			// create SQL update statement
+			String sql = "update estudiante "
+						+ "set nombre=?, apellido=?, email=?, escuela=? "
+						+ "where id=?";
+			
+			// prepare statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setString(1, theStudent.getNombre());
+			myStmt.setString(2, theStudent.getApellido());
+			myStmt.setString(3, theStudent.getEmail());
+			myStmt.setString(4, theStudent.getEscuela());
+			myStmt.setInt(5, theStudent.getId());
+			
+			// execute SQL statement
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC objects
+			close(myConn, myStmt, null);
+		}	
+		
+	}
+
+	public void deleteStudent(String theStudentId) throws Exception {
+
+		Connection myConn = null;
+		PreparedStatement myStmt = null;
+		
+		try {
+			// convert student id to int
+			int studentId = Integer.parseInt(theStudentId);
+			
+			// get connection to database
+			myConn = dataSource.getConnection();
+			
+			// create sql to delete student
+			String sql = "delete from estudiante where id=?";
+			
+			// prepare statement
+			myStmt = myConn.prepareStatement(sql);
+			
+			// set params
+			myStmt.setInt(1, studentId);
+			
+			// execute sql statement
+			myStmt.execute();
+		}
+		finally {
+			// clean up JDBC code
+			close(myConn, myStmt, null);
+		}
+	
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
